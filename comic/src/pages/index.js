a0a8d577123aebcf9a6fa7a6abf402ea581475b1
itc-s23019 <s23019@std.it-college.ac.jp>
2025-01-28 10:33:22 +0900

    ゴミ！！！！

--- comic/src/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-        <div style={{ padding: '20px' }}>
-            <h1>ホーム画面</h1>
-            <p>ここにメインコンテンツを表示します。</p>
-        </div>
-    );
-}
